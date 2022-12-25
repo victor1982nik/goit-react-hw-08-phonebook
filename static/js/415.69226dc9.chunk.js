9899bbed878424c14bf2c358864454fc0a90fcf9
victor1982nik <45390911+victor1982nik@users.noreply.github.com>
2022-12-25 12:22:21 +0000

    Deploying to gh-pages from @ victor1982nik/goit-react-hw-08-phonebook@56976bb7493d35af7e65c1562535ef715241b532 🚀

---
+++ static/js/415.69226dc9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,n,o){o.r(n),o.d(n,{default:function(){return a}});var t=o(111),r=o(184);function a(){return(0,r.jsx)(t.x,{fontSize:4,p:5,children:"This is site where you can manage your PhoneBook, all contacts are private!!!"})}}}]);
+//# sourceMappingURL=415.69226dc9.chunk.js.map
